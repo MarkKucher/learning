f8b97eaa1f869da4371370c6646854bef61cd4b4
MarkKucher <markkucher100@gmail.com>
2023-05-18 11:21:23 +0300

    I've added description and code example to create theme component

--- client/modules/redux/page/ReduxToolkit.tsx
+++ client/modules/redux/page/ReduxToolkit.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import TopicBlock from "@/modules/example/TopicBlock";
 import ExampleBlock from "@/modules/example/ExampleBlock";
 import CreateTheme from "@/modules/redux/page/examples/CreateTheme";
+import {reduxCodeExamples, reduxDescription} from "@/modules/redux/utils/reduxDescription";
+import CodeExample from "@/modules/example/CodeExample";
 
 const ReduxToolkit = () => {
     return (
         <TopicBlock title={"Redux toolkit"}>
-            <ExampleBlock title={"Create theme"}>
+            <ExampleBlock
+                descriptionContent={[reduxDescription.createTheme, <CodeExample code={reduxCodeExamples.createTheme} language={'js'}/>]}
+                title={"Create theme"}
+            >
                 <CreateTheme/>
             </ExampleBlock>
         </TopicBlock>
--- client/modules/redux/utils/reduxDescription.ts
+++ client/modules/redux/utils/reduxDescription.ts
@@ -1,3 +1,56 @@
 export const reduxDescription = {
-    CreateTheme: ''
+    createTheme: 'In this example, thanks to redux toolkit you can create theme to this  site. Additionally redux persist store your' +
+        ' theme in local storage. Here are also some error handlers.'
+}
+
+export const reduxCodeExamples = {
+    createTheme: 'const create = () => {\n' +
+        '        if(!name) {\n' +
+        '            setError(\'Theme must have name\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(themes.find(t => t.name === name)) {\n' +
+        '            setError(\'This name is already used\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(!isValidColor(mainGradient)) {\n' +
+        '            setError(\'Main gradient is not a color\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(!isValidColor(subGradient)) {\n' +
+        '            setError(\'Sub gradient is not a color\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(!isValidColor(bodyBackground)) {\n' +
+        '            setError(\'Body background is not a color\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(!isValidColor(text)) {\n' +
+        '            setError(\'Text color is not a color\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(!isValidColor(description)) {\n' +
+        '            setError(\'Description color is not a color\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(!isValidColor(mainScrollbarBackground)) {\n' +
+        '            setError(\'Main scrollbar background is not a color\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        if(!isValidColor(subScrollbarBackground)) {\n' +
+        '            setError(\'Sub scrollbar background is not a color\')\n' +
+        '            return null;\n' +
+        '        }\n' +
+        '        const theme: DefaultTheme = new Theme(name, mainGradient, subGradient, text, description, bodyBackground, mainScrollbarBackground, subScrollbarBackground, true)\n' +
+        '        dispatch(createTheme(theme))\n' +
+        '        setError(\'\')\n' +
+        '        setName(\'\')\n' +
+        '        setMainGradient(\'\')\n' +
+        '        setSubGradient(\'\')\n' +
+        '        setBodyBackground(\'\')\n' +
+        '        setText(\'\')\n' +
+        '        setDescription(\'\')\n' +
+        '        setMainScrollbarBackground(\'\')\n' +
+        '        setSubScrollbarBackground(\'\')\n' +
+        '    }'
 }
