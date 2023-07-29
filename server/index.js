62558d298ad28a83a36049850ea8f5ac8f0b4801
MarkKucher <markkucher100@gmail.com>
2023-07-29 17:26:39 +0300

    Payload is too large error fixing

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const fs = require('fs')
 const path = require('path')
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 let ids = [];
 
