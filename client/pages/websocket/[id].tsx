ef27cc5e8675d26d5a116dbb7b20fd98bbdb668c
MarkKucher <markkucher100@gmail.com>
2023-11-16 13:40:33 +0200

    wrong import, fixed

--- client/pages/websocket/[id].tsx
+++ client/pages/websocket/[id].tsx
@@ -2,11 +2,8 @@ import React, {useEffect} from 'react';
 import MainLayout from "@/components/layout/MainLayout";
 import Header from "@/modules/header/Header";
 import PaintOnline from "@/modules/websocket/page/PaintOnline";
-import {useParams} from "next/navigation";
-import {websocketServer} from "@/modules/websocket/page/utils/const";
-import {useDispatch, useSelector} from "react-redux";
-import {selectPaint, setSessionId, setSocket} from "@/modules/websocket/page/redux/paint";
-import Brush from "@/modules/websocket/page/tools/Brush";
+import {useDispatch} from "react-redux";
+import {setSessionId} from "@/modules/websocket/page/redux/paint";
 
 interface WebsocketProps {
     id: string;
