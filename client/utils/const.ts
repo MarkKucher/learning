a81b5346980e170e1b05c30165644ad5bff1881d
MarkKucher <markkucher100@gmail.com>
2023-11-20 16:53:40 +0200

    trying to use environmant varialbes

--- client/utils/const.ts
+++ client/utils/const.ts
@@ -1,4 +1,4 @@
 export const CLIENT = 'http://localhost:3000'
-export const serverUrl = 'https://learning-server-uwx4.onrender.com'
-export const wsServerUrl = 'https://learning-rosy.onrender.com'
-export const websocketServer = 'wss://learning-rosy.onrender.com'
+export const serverUrl = process.env.SERVER_URL || 'https://learning-server-uwx4.onrender.com'
+export const wsServerUrl = process.env.WS_SERVER_URL || 'https://learning-rosy.onrender.com'
+export const websocketServer = process.env.WEBSOCKET_SERVER || 'wss://learning-rosy.onrender.com'
