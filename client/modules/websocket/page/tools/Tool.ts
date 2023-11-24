f00562f233c563cf2868dc8c1673f5fd68571da5
MarkKucher <markkucher100@gmail.com>
2023-11-24 21:57:29 +0200

    returning some deps that are necessary

--- client/modules/websocket/page/tools/Tool.ts
+++ client/modules/websocket/page/tools/Tool.ts
@@ -17,6 +17,18 @@ export default class Tool {
         }))
     }
 
+    set fillColor(color: string) {
+        if(this.ctx) this.ctx.fillStyle = color
+    }
+
+    set strokeColor(color: string) {
+        if(this.ctx) this.ctx.strokeStyle = color
+    }
+
+    set lineWidth(width: number) {
+        if(this.ctx) this.ctx.lineWidth = width;
+    }
+
     destroyEvents() {
         if(this.canvas) {
             this.canvas.onmousedown = null;
