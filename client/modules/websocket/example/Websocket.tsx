c9aaaa617b871241975de69103ff0a9d3442774f
MarkKucher <markkucher100@gmail.com>
2024-03-27 23:35:55 +0200

    Uhm, can someone tell me how to deploy my server?

--- client/modules/websocket/example/Websocket.tsx
+++ client/modules/websocket/example/Websocket.tsx
@@ -1,37 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import styles from "@/modules/websocket/page/styles/Websocket.module.scss";
 import {websocketServer, wsServerUrl} from "@/utils/const";
+import axios from "axios";
 
 const Websocket = () => {
-    const [quantity, setQuantity] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [summand, setSummand] = useState(0);
+
+    const socket = new WebSocket(websocketServer + '/example');
 
     useEffect(() => {
-        let socket = new WebSocket(websocketServer + '/quantity')
-        const openHandler = () => {
-            socket.readyState === socket.OPEN && socket.send(JSON.stringify({method: 'open'}))
+        const getSumFromServer = async () => {
+            const response = await axios.get(wsServerUrl + '/example');
+            const sumFromServer = response.data
+            setSum(sumFromServer)
         }
-        const closeHandler = () => {
-            console.log('onclose')
-            socket.readyState === socket.OPEN && socket.send(JSON.stringify({method: 'close'}))
-        }
-        socket.onopen = () => {
-            openHandler()
-        }
-        window.onbeforeunload = () => {
-            closeHandler()
-        }
-        socket.onmessage = (e) => {
-            setQuantity(e.data)
-        }
-        return () => {
-            socket.close()
+
+        getSumFromServer()
+
+        socket.onmessage = (e ) => {
+            const {sum} = JSON.parse(e.data);
+            setSum(sum)
         }
     }, [])
 
+    const sendSummand = () => {
+        socket.send(JSON.stringify({summand}))
+        setSummand(0)
+    }
 
     return (
         <div className={styles.example}>
-            Visitors: {quantity}
+            <div>
+                Sum: {sum}
+            </div>
+            <input
+                type={'number'}
+                maxLength={4}
+                value={summand}
+                onChange={(e) => {setSummand(Number(e.target.value))}}
+            />
+            <button onClick={sendSummand}>
+                Add
+            </button>
         </div>
     );
 };
