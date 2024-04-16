8690834f8f50b8a772399a8e2de6b15335390dec
MarkKucher <markkucher100@gmail.com>
2024-04-16 17:27:55 +0300

    I have no clue why

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,8 +3,8 @@ import '@/styles/prism.scss';
 import type { AppProps } from 'next/app';
 import {Provider} from "react-redux";
 import configureStore from "@/modules/redux/store/configureStore";
-import { PersistGate } from 'redux-persist/integration/react';
-import { Manrope } from 'next/font/google';
+import { PersistGate } from 'redux-persist/integration/react'
+import { Manrope } from 'next/font/google'
 
 const manrope = Manrope({subsets: ['latin']});
 
--- client/utils/const.ts
+++ client/utils/const.ts
@@ -1,3 +1,3 @@
-export const serverUrl = 'http://localhost:5000' //process.env.SERVER_URL || 'https://learning-server-uwx4.onrender.com'
+export const serverUrl = process.env.SERVER_URL || 'https://learning-server-uwx4.onrender.com'
 export const wsServerUrl = process.env.WS_SERVER_URL || 'https://learning-rosy.onrender.com'
 export const websocketServer = process.env.WEBSOCKET_SERVER || 'wss://learning-rosy.onrender.com'
