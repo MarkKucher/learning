1dd03b274d74e05ee546c444fa1100cdfa649c63
MarkKucher <markkucher100@gmail.com>
2024-04-16 15:42:05 +0300

    global styles can not be imported anywhere other than in _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,10 +1,11 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import '@/styles/prism.scss';
+import type { AppProps } from 'next/app';
 import {Provider} from "react-redux";
 import configureStore from "@/modules/redux/store/configureStore";
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 import LogRocket from 'logrocket';
-import { Manrope } from 'next/font/google'
+import { Manrope } from 'next/font/google';
 
 const manrope = Manrope({subsets: ['latin']});
 
