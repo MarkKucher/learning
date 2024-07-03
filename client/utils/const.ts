0d7761001a20bfef3b43864ed97dcfbe6641b0dd
MarkKucher <markkucher100@gmail.com>
2024-07-03 13:52:35 +0300

    new ws server

--- client/utils/const.ts
+++ client/utils/const.ts
@@ -1,3 +1,3 @@
 export const serverUrl = process.env.SERVER_URL || 'https://learning-426008.oa.r.appspot.com'
-export const wsServerUrl = process.env.WS_SERVER_URL || 'https://learning-rosy.onrender.com'
-export const websocketServer = process.env.WEBSOCKET_SERVER || 'wss://learning-rosy.onrender.com'
+export const wsServerUrl = process.env.WS_SERVER_URL || 'https://learning-ws.ew.r.appspot.com'
+export const websocketServer = process.env.WEBSOCKET_SERVER || 'wss://learning-ws.ew.r.appspot.com'
