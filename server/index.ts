02d60223f1607bee4dec0021b780b0b08d3a63fa
MarkKucher <markkucher100@gmail.com>
2024-07-12 18:25:16 +0300

    origins

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,11 @@ import {router} from "./router";
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://learning-rosy.verce.app']
+}
+
+app.use(cors(corsOptions))
 app.use(json())
 app.use(urlencoded({ extended: true }));
 
