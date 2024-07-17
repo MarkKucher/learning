e252528ac21ac02c726f1839ac4724f59a1c9ba9
MarkKucher <markkucher100@gmail.com>
2024-07-17 14:39:21 +0300

    new ws-server

--- client/utils/const.ts
+++ client/utils/const.ts
@@ -1,3 +1,3 @@
 export const serverUrl = process.env.SERVER_URL || 'https://learning-production-1984.up.railway.app'
-export const wsServerUrl = process.env.WS_SERVER_URL || 'https://learning-ws.ew.r.appspot.com'
-export const websocketServer = process.env.WEBSOCKET_SERVER || 'wss://learning-ws.ew.r.appspot.com'
+export const wsServerUrl = process.env.WS_SERVER_URL || 'https://learning-production-82c7.up.railway.app'
+export const websocketServer = process.env.WEBSOCKET_SERVER || 'wss://learning-production-82c7.up.railway.app'
