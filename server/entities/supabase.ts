41b22fe278abc6be20c09908a1f44683fe9101cb
MarkKucher <markkucher100@gmail.com>
2024-07-19 14:01:19 +0300

    minor issues

--- server/entities/supabase.ts
+++ server/entities/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = 'https://ybcowivznkuauyibygeq.supabase.co';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InliY293aXZ6bmt1YXV5aWJ5Z2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MDkxNzksImV4cCI6MjAxMjA4NTE3OX0.MsYE1-Hd4rFzUxsbEFGHR52hKfpzpwn4I-lOMPv8HNM';
+
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey, {
     auth: { persistSession: false },
