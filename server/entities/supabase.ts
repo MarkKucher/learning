5dccddff92c796446e3c9af192f4d265d2f925d0
MarkKucher <markkucher100@gmail.com>
2024-07-19 14:08:17 +0300

    maybe this was not the case

--- server/entities/supabase.ts
+++ server/entities/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = 'https://ybcowivznkuauyibygeq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InliY293aXZ6bmt1YXV5aWJ5Z2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MDkxNzksImV4cCI6MjAxMjA4NTE3OX0.MsYE1-Hd4rFzUxsbEFGHR52hKfpzpwn4I-lOMPv8HNM';
-
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey, {
     auth: { persistSession: false },
