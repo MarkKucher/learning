d0b779c6d4094d548e57e84fb32c3f565ec65c73
MarkKucher <markkucher100@gmail.com>
2025-03-26 00:44:45 +0100

    bla

--- server/entities/supabase.ts
+++ server/entities/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = 'https://hgrdcvsykfndbuopbhfj.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhncmRjdnN5a2ZuZGJ1b3BiaGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NDAzODMsImV4cCI6MjA1ODUxNjM4M30.TA-1po97h5RaGoNxzMtSar4TgN4nXoobezXjEg9KFDM" || '';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhncmRjdnN5a2ZuZGJ1b3BiaGZqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Mjk0MDM4MywiZXhwIjoyMDU4NTE2MzgzfQ.Rs5-VxlyG0XjQRMG4F8HSAtHpDn7Y2RZwGqGAJ6C43g" || '';
 
 export const supabase = createClient(supabaseURL, supabaseKey, {
     auth: { persistSession: false },
