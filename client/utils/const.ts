04d49051d13fb34823fe6f7046ac3556116d783d
MarkKucher <markkucher100@gmail.com>
2025-03-26 00:03:04 +0100

    meh

--- client/utils/const.ts
+++ client/utils/const.ts
@@ -1,3 +1,3 @@
-export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'https://learning-production-42d9.up.railway.app'//'http://localhost:8000'
+export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8000'
 export const wsServerUrl = process.env.NEXT_PUBLIC_WS_SERVER_URL || 'https://learning-production-82c7.up.railway.app' //'http://localhost:5000'
 export const websocketServer = process.env.NEXT_PUBLIC_WEBSOCKET_SERVER || 'wss://learning-production-82c7.up.railway.app'//'ws://localhost:5000'
