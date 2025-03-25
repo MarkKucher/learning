0a59eb35054bd04484580c4cd29a3459e7225e8f
MarkKucher <markkucher100@gmail.com>
2025-03-26 00:27:40 +0100

    that's driving me crazy

--- server/entities/supabase.ts
+++ server/entities/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = 'https://hgrdcvsykfndbuopbhfj.supabase.co';
-const supabaseKey = process.env.SUPABASE_ANON_KEY || '';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhncmRjdnN5a2ZuZGJ1b3BiaGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NDAzODMsImV4cCI6MjA1ODUxNjM4M30.TA-1po97h5RaGoNxzMtSar4TgN4nXoobezXjEg9KFDM" || '';
 
 export const supabase = createClient(supabaseURL, supabaseKey, {
     auth: { persistSession: false },
